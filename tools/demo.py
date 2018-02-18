df8da3d90771dbf56f05b713204ca1806e52c664
Abhinav-Reddy <abhinavpodduturi@ufl.edu>
2018-02-18 21:38:30 +0000

    Q1 results

--- tools/demo.py
+++ tools/demo.py
@@ -143,8 +143,8 @@ if __name__ == '__main__':
     for i in xrange(2):
         _, _= im_detect(net, im)
 
-    im_names = ['000456.jpg', '000542.jpg', '001150.jpg',
-                '001763.jpg', '004545.jpg']
+    im_names = ['1.jpg', '2.jpg', '3.jpg',
+                '4.jpg', '5.jpg', '6.jpg']
     for im_name in im_names:
         print '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
         print 'Demo for data/demo/{}'.format(im_name)
